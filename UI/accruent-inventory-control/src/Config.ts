18188c4be8909bf3c7ba9f104b049589a7bbfffd
Marcos Antônio Marcon <diel.mormac@gmail.com>
2025-03-24 14:40:09 -0300

    Update Config.ts

--- UI/accruent-inventory-control/src/Config.ts
+++ UI/accruent-inventory-control/src/Config.ts
@@ -1,5 +1,5 @@
 const config = {
-    apiBaseUrl: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5116/api/v1', // Default to localhost if not set
+    apiBaseUrl: 'http://localhost:5116/api/v1', // Default to localhost if not set
     timeout: 10000, // Timeout for API requests
 };
 
